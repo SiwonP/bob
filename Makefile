191c3a78e5e56e9ea561a8edd7adf8c9094dfca2
Simon Petit <nomisp96@hotmail.fr>
2024-10-26 16:39:45 +0200

    wip

--- Makefile
+++ Makefile
@@ -2,13 +2,14 @@ install:
 	@echo "Installing bob"
 	cp bob /usr/local/bin
 	cp bob2 /usr/local/bin
-	cp -r lib /usr/local/lib/bob
+	cp -r lib/template /usr/local/lib/bob/template	
+	cp lib/*.awk /usr/local/lib/bob
 
 uninstall:
 	@echo "Uninstalling bob"
 	rm /usr/local/bin/bob
 	rm /usr/local/bin/bob2
-	rm -r /usr/local/lib/bob
+	rm -rf /usr/local/lib/bob
 
 .PHONY: test  # Declare 'test' as a phony target
 
--- test/test.sh
+++ test/test.sh
@@ -8,4 +8,26 @@ NC='\033[0m' # No Color (reset)
 
 echo -e "${YELLOW}Testing markown parser :${NC}\n"
 
-./parser/test_md_parser.sh ../lib/markdown.awk awk
+./test/parser/test_md_parser.sh ./lib/markdown.awk awk
+
+echo -e "${YELLOW}Testing bob init :${NC}\n"
+
+author="author"
+blog="blog"
+lang="fr"
+dark="y"
+
+./bob2 init << EOF
+$author
+$blog
+$lang
+$dark
+EOF
+
+conf_author=$(grep "^author=" .blog.conf | cut -d '=' -f 2)
+conf_blog=$(grep "^blog=" .blog.conf | cut -d '=' -f 2)
+conf_lang=$(grep "^lang=" .blog.conf | cut -d '=' -f 2)
+conf_dark=$(grep "^dark=" .blog.conf | cut -d '=' -f 2)
+
+rm -r posts
+rm -r drafts
